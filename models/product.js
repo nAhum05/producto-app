56d2f0757ffc5c86c3b4a0d8fbba800b045532cf
Nahum <24300712@uttt.edu.mx>
2025-03-03 09:08:38 -0600

    agregar-mongoDB

---
+++ models/product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+// Esquema para los productos
+const productSchema = new mongoose.Schema({
+name: {
+type: String,
+required: true
+},
+price: {
+type: Number,
+required: true
+}
+});
+// Modelo de producto
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
